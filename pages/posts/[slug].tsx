eb37844726f4063bf8cfa7dcfe717e0fc1f40dbf
Brent Mitton <bmitton@gatech.edu>
2020-06-08 13:44:39 -0400

    markdownify and start putting inline in

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,15 +3,42 @@ import PostBody from "../../components/post-body";
 import { fetchGraphql } from "../../lib/api";
 import Header from "../../components/header";
 import Head from "next/head";
+import { useForm, usePlugin, FormOptions } from "tinacms";
+import { useMemo, useEffect, useState } from "react";
+import markdownToHtml from "../../lib/markdownToHtml";
+import { InlineForm, InlineText } from "react-tinacms-inline";
 
-export default function ({ post, preview }) {
+export default function ({ post: initialPost, preview }) {
+  const formConfig: any = {
+    id: initialPost.slug,
+    label: "Blog Post",
+    initialValues: initialPost,
+    fields: [
+      {
+        name: "title",
+        label: "Post Title",
+        component: "text",
+      },
+    ],
+  };
+  const [post, form] = useForm(formConfig);
+  usePlugin(form);
+  const initialContent = useMemo(() => post.content, []);
+  const [htmlContent, setHtmlContent] = useState(post.content);
+
+  useEffect(() => {
+    markdownToHtml(post.content).then(setHtmlContent);
+  }, [post.content]);
+
   return (
     <PostLayout preview={preview}>
       <Head>
-        <title>{post.title} | Tina Strapi Example</title>
+        <title>{initialPost.title} | Tina Strapi Example</title>
       </Head>
-      <Header>{post.title}</Header>
-      <PostBody content={post.content} />
+      <InlineForm form={form}>
+        <InlineText name="title" />
+      </InlineForm>
+      <PostBody content={htmlContent} />
     </PostLayout>
   );
 }
