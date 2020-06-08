a9fb9c27cc0944f50e744b661ae8e4fe4967a9b0
Brent Mitton <bmitton@gatech.edu>
2020-06-08 13:52:05 -0400

    stop trying to show the sidebar, inline only

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { TinaCMS, TinaProvider } from "tinacms";
 
 export default function MyApp({ Component, pageProps }) {
-  const cms = new TinaCMS({});
+  const cms = new TinaCMS({ sidebar: { hidden: true } });
   return (
     <TinaProvider cms={cms}>
       <Component {...pageProps} />
