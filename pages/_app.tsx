f500f58e286dc64e63858e839207054ed1821d7b
Brent Mitton <bmitton@gatech.edu>
2020-06-09 10:03:35 -0400

    add the basic auth button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,45 @@
 import { TinaCMS, TinaProvider } from "tinacms";
 
+import { StrapiProvider } from "../components/tina-strapi/strapi-provider";
+import { useStrapiEditing } from "../components/tina-strapi/use-strapi-editing";
+
 export default function MyApp({ Component, pageProps }) {
   const cms = new TinaCMS({ sidebar: { hidden: true } });
   return (
     <TinaProvider cms={cms}>
-      <Component {...pageProps} />
+      <StrapiProvider
+        editMode={pageProps.preview}
+        enterEditMode={enterEditMode}
+        exitEditMode={exitEditMode}
+      >
+        <EditButton editMode={pageProps.preview} />
+        <Component {...pageProps} />
+      </StrapiProvider>
     </TinaProvider>
   );
 }
+
+export interface EditButtonProps {
+  editMode: boolean;
+}
+
+export const EditButton = ({ editMode }: EditButtonProps) => {
+  const strapi = useStrapiEditing();
+  return (
+    <button onClick={editMode ? strapi.exitEditMode : strapi.enterEditMode}>
+      {editMode ? "Stop Editing" : "Edit This Site"}
+    </button>
+  );
+};
+
+const enterEditMode = () => {
+  return fetch(`/api/preview`).then(() => {
+    window.location.href = window.location.pathname;
+  });
+};
+
+const exitEditMode = () => {
+  return fetch(`/api/reset-preview`).then(() => {
+    window.location.reload();
+  });
+};
