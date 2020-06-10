33ce60db19805b4d2618ce667a15761d3775d0c0
Brent Mitton <bmitton@gatech.edu>
2020-06-10 08:58:32 -0400

    pass auth header, saving now works

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,25 @@
+import {
+  STRAPI_JWT,
+  STRAPI_URL,
+} from "../components/tina-strapi/tina-strapi-client";
+
+import Cookies from "js-cookie";
 import axios from "axios";
-import { STRAPI_URL } from "../components/tina-strapi/tina-strapi-client";
 
 export const GRAPHQL_ENDPOINT = `${STRAPI_URL}/graphql`;
 
 export async function fetchGraphql(query, variables = {}) {
+  const jwt = Cookies.get(STRAPI_JWT);
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  if (jwt) headers["Authorization"] = `Bearer ${jwt}`;
+
   const response = await axios.post(
     GRAPHQL_ENDPOINT,
     { query: query, variables: variables },
-    { headers: { "Content-Type": "application/json" } }
+    { headers: { ...headers } }
   );
 
   return response.data;
